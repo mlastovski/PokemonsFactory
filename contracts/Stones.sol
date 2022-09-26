48078a2112aa376ab8ac316b8d108e8489c64c11
Mark Lastovskyi <emloft@protonmail.com>
2022-09-26 23:56:51 +0300

    Stones.sol v1

--- contracts/Stones.sol
+++ contracts/Stones.sol
@@ -1,28 +1,27 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.11;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
+/// @title An ERC-1155 compliant token for the PokemonsFactory project.
+/// @author mlastovski
 contract Stones is ERC1155, AccessControl {
+    /// @dev Role that will be able to call `mintStone` and `burnStone`.
     bytes32 public constant MANIPULATOR_ROLE = keccak256("MANIPULATOR_ROLE");
 
+    /// @dev A string with the IPFS location of token assets.
     string public ipfsLocation;
 
+    /// @dev Name of the collection (assigned to correctly display on OpenSea).
     string public name = "Stones";
 
+    /// @dev Symbol of the collection (assigned to correctly display on OpenSea).
     string public symbol = "STNS";
 
-    uint256 public constant THUNDER = 1;
-
-    uint256 public constant ICE = 2;
-
-    uint256 public constant MOON = 3;
-
-    uint256 public constant FIRE = 4;
-
+    /// @dev Grants an admin role to the contract creator.
+    /// @param _ipfsLocation A string with the IPFS location of token assets.
     constructor(
         string memory _ipfsLocation
     ) 
@@ -35,6 +34,9 @@ contract Stones is ERC1155, AccessControl {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
     }
 
+    /// @dev Mint tokens from `id` collection to the address `to`.
+    /// @param to Address to mint tokens to.
+    /// @param id Id of the collection.
     function mintStone(
         address to, 
         uint256 id
@@ -45,6 +47,9 @@ contract Stones is ERC1155, AccessControl {
         _mint(to, id, 1, "");
     }
 
+    /// @dev Burn tokens from `id` collection from the address `from`.
+    /// @param from Address to burn tokens from.
+    /// @param id Id of the collection.
     function burnStone(
         address from, 
         uint256 id
@@ -55,10 +60,20 @@ contract Stones is ERC1155, AccessControl {
         _burn(from, id, 1);
     }
 
-    function initialize(address target) external onlyRole(DEFAULT_ADMIN_ROLE) {
+    /// @dev Grants role `MANIPULATOR_ROLE` to the address `target`.
+    /// @param target Address to give role to.
+    function initialize(
+        address target
+    ) 
+        external 
+        onlyRole(DEFAULT_ADMIN_ROLE) 
+    {
         _grantRole(MANIPULATOR_ROLE, target);
     }
 
+    /// @notice Sets NFT matadata (e.g. for OpenSea).
+    /// @param _tokenId Token's ID.
+    /// @return A string with NFT metadata. 
     function uri(
         uint256 _tokenId
     ) 
@@ -74,6 +89,8 @@ contract Stones is ERC1155, AccessControl {
         );
     }
 
+    /// @dev Used to resolve the conflict when inheriting from both
+    ///      AccessControl and ERC1155.
     function supportsInterface(
         bytes4 interfaceId
     )
